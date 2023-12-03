1ac54695b12875c26ff2ae42de59541ed4f25760
S3B4Z1MM <70818874+S3B4Z1MM@users.noreply.github.com>
2023-12-03 08:40:23 +0100

    Update day2.py
    adds calculate power from a minimal set to solve part 2

--- day2/python/day2.py
+++ day2/python/day2.py
@@ -1,3 +1,5 @@
+import re
+
 def is_game_possible(game, cube_counts):
     for subset in game[0]:
         for cube in subset.split(','):
@@ -15,6 +17,32 @@ def find_possible_games(games, cube_counts):
             possible_games.append(int(game_id.strip().split(' ')[-1]))
     return possible_games
 
+def calculate_power_of_min_set_of_cubes(games):
+    min_sets = []
+    for game in games:
+        product = 1
+        min_set = {"red":0,"green":0,"blue":0}
+        subsets = game.split(':')[-1]
+        # Use re.split() to split using both ',' and ';'
+        colors = re.split(r'[,;]', subsets)
+
+        # Filter out empty strings and trim whitespace
+        colors = [color.strip() for color in colors if color.strip()]
+        
+        for color in colors:
+            cube, color = color.split()
+            cube = int(cube)
+            if (min_set.get(color) > cube):
+                continue
+            
+            min_set.update({color: cube})
+ 
+        for color, cube in min_set.items():
+            product *= cube
+        min_sets.append(product)
+
+    return min_sets
+
 # Cube configuration
 cube_counts = {
     'red': 12,
@@ -25,8 +53,13 @@ cube_counts = {
 # Find possible games
 with open('day2/input.txt') as games_input:
     possible_games = find_possible_games(games_input, cube_counts)
-
+    
 # Calculate the sum of IDs of possible games
 sum_of_ids = sum(possible_games)
 
 print("Sum of IDs of possible games:", sum_of_ids)
+
+with open('day2/input.txt') as games_input:
+    power_of_min_set_of_cubes = calculate_power_of_min_set_of_cubes(games_input)
+
+print(f'Power of the minimal sets are: {sum(power_of_min_set_of_cubes)}')
